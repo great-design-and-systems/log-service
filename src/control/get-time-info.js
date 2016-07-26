1709514dc06a04b1f24001ad20ea7d5b203d3480
Analyn Flores <analynflores@gmail.com>
2016-07-27 01:16:40 +0800

    new api: get-time-info

---
+++ src/control/get-time-info.js
@@ -0,0 +1,19 @@
+'use strict';
+var TimeIn = require('../entity/TimeIn');
+
+function execute(timeInID, callback) {
+    TimeIn.findById(timeInID, 'fullname studentLevel department studentLevel when', 
+        function(err, result) {
+            if (err) {
+                callback(err);
+            } else {
+                if (result) {
+                    callback(null, result);
+                } else {
+                    callback(null, {message: 'No record found.'});
+                }
+            }
+        });
+}
+
+module.exports = execute;
